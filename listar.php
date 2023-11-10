bea29d3b0586a563ef20dd45836f615721bee2ac
Pedro1237 <105647384+Pedro1237@users.noreply.github.com>
2023-11-10 08:47:35 -0300

    Update listar.php

--- listar.php
+++ listar.php
@@ -1,87 +1,52 @@
-<!DOCTYPEhtml>
+<!DOCTYPE html>
+<html lang="en">
 
-<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Reservas</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+</head>
 
-<cabeça>
+<body>
+    <h1 class="title is-3">Listagem de Reservas</h1>
+    <?php
+    require "Conectar.php";
+    require "Reserva.php";
+    require "RepositorioReserva.php";
 
-<meta charset="UTF-8">
-
-<meta name="viewport" content="escala de largura=1.0">
-
-<title>Listar Reservas</title>
-
-<link rel="folha de estilo"
-
-href="https://cdn.jsdelivr.net/npm/bulma( </head>
-
-<corpo>
-
-<h1 class="title is-3">Listagem de Reser
-
-<?php
-
-requer "Connect.php"; requer "Reserva.php"; requer "ReservationRepository.php";
-
-$valores=(newReservationRepository())-> ?>
+    $valores = (new RepositorioReserva())->exibirTudo($banco);
+    ?>
 <table class="table is-striped">
-
-<tr>
-
-<td>ID</td>
-
-<td>Nome</td>
-
-<td>Check-in</td>
-
-<td>Finalizar compra</td>
-
-<td>Número de Hospedes</td>
-
-</tr>
-
-<?php foreach ($valores as $valor): ?>
-
-<tr>
-
-<td>
-
-<?= ($valor['id']) ?>
-
-</td>
-
-<td>
-
-<?= ($valor['nome']) ?>
-
-</td>
-
-<td>
-
-<?= ($valor['checkin']) ?>
-
-</td>
-
-<td>
-
+        <tr>
+            <td>ID</td>
+            <td>Nome</td>
+            <td>Checkin</td>
+            <td>Checkout</td>
+            <td>Número de Hospedes</td>
+        </tr>
+        <?php foreach ($valores as $valor): ?>
+            <tr>
+                <td>
+                    <?= ($valor['id']) ?>
+                </td>
+                <td>
+                    <?= ($valor['nome']) ?>
+                </td>
+                <td>
+                    <?= ($valor['checkin']) ?>
+                </td>
+                <td>
 <?= ($valor['checkout']) ?>
-
-</td>
-<td>
-
-<?= ($valor['num_hospedes']) ?>
-
-</td>
-
-</tr>
-
-<?php endforeach; ?>
-
-</tabela>
-
-<br>
-
-<a class="button is-primary" href="inde Reserva</a>
-
+                </td>
+                <td>
+                    <?= ($valor['num_hospedes']) ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <br>
+    <a class="button is-primary" href="index.php">Cadastrar Reserva</a>
 </body>
 
 </html>
